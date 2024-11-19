8262edb486c0ae646519e5eb19e1a8edfcd5cc5e
Marco Di Fiandra <marcdifiandra98@gmail.com>
2024-11-20 00:07:51 +0100

    Modificato il gitignore, aggiunto le classi retriver e hub e modifica alle configurazione

---
+++ dataRetriver.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import configparser as cp
+
+class Retriver:
+
+    def __checkCSVCompatibility(self,Colums):
+       field_value = self.config.get('CSVCONFIG', 'colums').split(',')
+       if not set(Colums).issubset(set(field_value)):
+          raise Exception("Not compatible Fields") 
+
+    def __init__(self,csv_path):
+        self.csv_df = None
+        self.config = cp.ConfigParser()
+        self.config.read('configs/config.cfg')
+        try:
+         self.csv_df = pd.read_csv(csv_path)
+         self.__checkCSVCompatibility( self.csv_df.columns)
+        except Exception as e:
+            print(str(e))
+            exit()
+
+    def getCsvData(self):
+       return self.csv_df
+    
---
+++ hub.py
@@ -0,0 +1,53 @@
+import configparser as cp
+import pandas as pd
+from dataRetriver import *
+import webbrowser
+import tkinter as tk
+from functools import partial
+from tkinter import PhotoImage
+from PIL import Image,ImageTk
+
+class Hub:
+
+    def __init__(self):
+        self.config = cp.ConfigParser()
+        self.config.read('configs/config.cfg')
+        self.d_r = Retriver(self.config.get("PATHS","csv_path"))
+
+    def __clickEvent(self,record):
+        print(f"You selected {record["Name"]}")
+        #webbrowser.open(record["Link"])
+
+    def __create_record_widgets(self,parent):
+        df = self.d_r.getCsvData()
+        for record in df.itertuples(index=False):
+            if record.Image == "nan" or record.Image == None:
+                image = PhotoImage(record.Image)
+            else:
+              image = PhotoImage(self.config.get("PATHS","placeholder_path"))  
+            button = tk.Button(
+            parent,
+            text=f"{record.Name}\n{record.Description}",
+            image=image,
+            compound="top",  # Immagine sopra al testo
+            command=partial(self.__clickEvent, record),
+            width=20,
+            height=100  # Imposta un'altezza fissa se necessario
+        )
+        #button.image = image  # Conserva una riferimento all'immagine per evitare che venga distrutta dal garbage collector
+        button.pack(pady=5, fill="x", padx=10)  # Usa 'fill="x"' per far sì che il pannello si adatti
+
+
+    def create_panel(self):
+        root = tk.Tk()
+        root.title("Company Hub")
+        root.geometry("400x300")  # Imposta una dimensione per la finestra, se necessario
+        # Crea un pannello (Frame) per contenere i record
+        panel = tk.Frame(root)
+        panel.pack(padx=10, pady=10, fill="both", expand=True)
+        self.__create_record_widgets(panel)
+        root.mainloop()
+
+hub = Hub()
+
+hub.create_panel()
